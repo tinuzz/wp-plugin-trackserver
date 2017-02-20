13af74611064b5d84988ee6acd34e4fc3b1d7a11
Martijn Grendelman <m@rtijn.net>
2017-02-20 23:09:34 +0100

    Don't try to delete tables from multiple blogs in a network when uninstalling.

--- uninstall.php
+++ uninstall.php
@@ -12,16 +12,4 @@ function delete_tables() {
 
 delete_option( 'trackserver_options' );
 delete_site_option( 'trackserver_options' );
-
-if ( function_exists( 'is_multisite' ) && is_multisite() ) {
-	global $wpdb;
-	$old_blog =  $wpdb->blogid;
-	$blogids =  $wpdb->get_col( "SELECT blog_id FROM $wpdb->blogs" );
-	foreach ( $blogids as $blog_id ) {
-		switch_to_blog($blog_id);
-		delete_tables();
-	}
-	switch_to_blog( $old_blog );
-} else {
-	delete_tables();
-}
+delete_tables();
