7ad76e93dcec5f02a96a6c818c6c31c9703fac7d
Martijn Grendelman <m@rtijn.net>
2019-09-30 00:06:15 +0200

    Add experimental class implementing the uLogger protocol

---
+++ class-trackserver-ulogger.php
@@ -0,0 +1,207 @@
+<?php
+
+if ( ! defined( 'TRACKSERVER_PLUGIN_DIR' ) ) {
+	die( 'No, sorry.' );
+}
+
+require_once TRACKSERVER_PLUGIN_DIR . 'class-trackserver-track.php';
+require_once TRACKSERVER_PLUGIN_DIR . 'class-trackserver-location.php';
+
+class Trackserver_Ulogger {
+
+	private $trackserver;  // Reference to the calling object
+	private $user;         // WP_User doing the request
+
+	/**
+	 * Constructor.
+	 *
+	 * @since 4.4
+	 */
+	public function __construct( $trackserver ) {
+		$this->trackserver = $trackserver;
+	}
+
+	/**
+	 * Handle a request from uLogger.
+	 *
+	 * Based on the value of $_POST['action'], delegate the actual work to an
+	 * appropriate function.
+	 *
+	 * @since 4.4
+	 */
+	public function handle_request() {
+		switch ( $_POST['action'] ) {
+			case 'auth':
+				$this->handle_auth();
+				break;
+			case 'addtrack':
+				$this->handle_addtrack();
+				break;
+			case 'addpos':
+				$this->handle_addpos();
+				break;
+			default:
+				$this->send_response( array( 'message' => 'Unknown command' ) );
+		}
+	}
+
+	/**
+	 * Terminate the request with a 401 status.
+	 *
+	 * @since 4.4
+	 */
+	private function require_auth() {
+		// The reference server does this, I'm not sure to what end.
+		//header( 'WWW-Authenticate: OAuth realm="users@ulogger"' );
+		header( 'HTTP/1.0 401 Unauthorized' );
+		die( "Authentication required\n" );
+	}
+
+	/**
+	 * Generate a JSON response for uLogger.
+	 *
+	 * It takes an associative array, to be sent as JSON. If the array contains a key
+	 * named 'message', it will be regarded as an error condition and the 'error' key
+	 * in the response will be set to 'true'.
+	 *
+	 * @since 4.4
+	 */
+	private function send_response( $data = array() ) {
+		$response          = array();
+		$response['error'] = ( array_key_exists( 'message', $data ) ? true : false );
+		header('Content-Type: application/json');
+		echo json_encode( array_merge( $response, $data ) );
+	}
+
+	/**
+	 * Handle 'auth' action.
+	 *
+	 * Verify the username and password sent in the POST request against
+	 * WordPress credentials.  When valid, start a session, store the user ID in
+	 * it and send a succesful response.
+	 *
+	 * @since 4.4
+	 */
+	private function handle_auth() {
+		if ( array_key_exists( 'user', $_POST ) && array_key_exists( 'pass', $_POST ) ) {
+			$user_id = $this->trackserver->validate_wp_user_pass( $_POST['user'], $_POST['pass'] );
+			if ( $user_id !== false ) {
+				session_start();
+				$_SESSION            = array();
+				$_SESSION['trackserver_user_id'] = $user_id;
+				return $this->send_response();
+				//return $this->send_response( array( "user_id" => $user_id ) );  // for debugging
+			}
+		}
+		$this->require_auth();
+	}
+
+	/**
+	 * Get user ID from session.
+	 *
+	 * If a user_id from Trackserver is present in the session. return it.
+	 * Otherwise, terminate the request.
+	 *
+	 * @since 4.4
+	 */
+	private function session_user() {
+		session_start();
+		if ( isset( $_SESSION['trackserver_user_id'] ) ) {
+			return $_SESSION['trackserver_user_id'];
+		}
+		$this->require_auth();
+	}
+
+	/**
+	 * Handle 'addtrack' action.
+	 *
+	 * @since 4.4
+	 */
+	private function handle_addtrack() {
+
+		// If this function returns, we're OK.
+		$user_id = $this->session_user();
+
+		if ( empty( $_POST['track'] ) ) {
+			return $this->send_response( array( 'message' => 'Missing required parameter' ) );
+		}
+
+		$track = new Trackserver_Track( $this->trackserver, null, $user_id );
+		$track->set( 'name', $_POST['track'] );
+
+		if ( array_key_exists( 'HTTP_USER_AGENT', $_SERVER ) ) {
+			$track->set( 'source', strtok( $_SERVER['HTTP_USER_AGENT'], ';' ) );
+		}
+		else {
+			$track->set( 'source', 'uLogger' );
+		}
+
+		if ( $track_id = $track->save() ) {
+			return $this->send_response( array( 'trackid' => $track_id ) );
+		}
+		else {
+			return $this->send_response( array( 'message' => 'Server error' ) );
+		}
+	}
+
+	/**
+	 * Handle 'addpos' action.
+	 *
+	 * @since 4.4
+	 */
+	private function handle_addpos() {
+		$user_id = $this->session_user();
+		$track   = new Trackserver_Track( $this->trackserver, $_POST['trackid'], $user_id );
+
+		if ( $track->id ) {
+
+			// For privileged users, $track->user_id could be different, so check if
+			// the current login is actually the owner of the track.
+			if ( (int) $track->user_id === (int) $user_id ) {
+
+				if ( ! ( empty( $_POST['lat'] ) || empty( $_POST['lon'] ) || empty( $_POST['time'] ) ) ) {
+
+					$loc = new Trackserver_Location( $this->trackserver, $track->id );
+
+					$ts   = (int) $_POST['time'];
+					$ts  += $this->trackserver->utc_to_local_offset( $ts );
+
+					$loc->set( 'latitude', $_POST['lat'] );
+					$loc->set( 'longitude', $_POST['lon'] );
+					$loc->set( 'occurred', date( 'Y-m-d H:i:s', $ts ) );
+
+					if ( ! empty ( $_POST['altitude'] ) ) {
+						$loc->set( 'altitude', $_POST['altitude'] );
+					}
+					if ( ! empty ( $_POST['speed'] ) ) {
+						$loc->set( 'speed', $_POST['speed'] );
+					}
+					if ( ! empty ( $_POST['bearing'] ) ) {
+						$loc->set( 'heading', $_POST['bearing'] );
+					}
+
+					$fenced = $this->trackserver->is_geofenced( $user_id, $data );
+					if ( $fenced == 'discard' ) {
+						return $this->send_response();
+					}
+					$loc->set( 'hidden', ( $fenced == 'hide' ? 1 : 0 ) );
+
+					if ( $loc->save() ) {
+						//$track->calculate_distance();  // not implemented yet
+						$this->trackserver->calculate_distance( $track_id );
+						return $this->send_response();
+					} else {
+						return $this->send_response( array( 'message' => 'Server error' ) );
+					}
+				} else {
+					return $this->send_response( array( 'message' => 'Missing required parameter' ) );
+				}
+			} else {
+				return $this->send_response( array( 'message' => 'Illegal track ID' ) );
+			}
+		} else {
+			return $this->send_response( array( 'message' => 'Unknown track ID' ) );
+		}
+	}
+
+}  // class
