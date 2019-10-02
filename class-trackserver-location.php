c1fa91d57b457cdba5c235c33206abb5d0ad9421
Martijn Grendelman <m@rtijn.net>
2019-10-02 23:41:08 +0200

    Build geofencing into Trackserver_Location class

--- class-trackserver-location.php
+++ class-trackserver-location.php
@@ -1,7 +1,7 @@
 <?php
 
 if ( ! defined( 'TRACKSERVER_PLUGIN_DIR' ) ) {
-	  die( 'No, sorry.' );
+	die( 'No, sorry.' );
 }
 
 class Trackserver_Location {
@@ -19,31 +19,66 @@ class Trackserver_Location {
 	private $comment   = '';
 	private $hidden    = 0;
 
-	// Trackserver object
 	private $trackserver;
+	private $user_id;
 
 	/**
 	 * Initialize the instance.
 	 *
 	 * @since 4.4
 	 */
-	public function __construct( $trackserver, $track_id = null ) {
+	public function __construct( $trackserver, $track_id = null, $user_id = null ) {
 		$this->trackserver = $trackserver;
 		$this->track_id    = $track_id;
+		$this->user_id     = $user_id;   // Needed for geofencing
 	}
 
 	/**
-	 * Save the track to the database.
+	 * Function to check whether this location is geofenced.
 	 *
-	 * If the instance doesn't have an ID, insert a new track, otherwise update
-	 * the existing track. Returns the track ID on success, or false on failure.
+	 * @since 3.1
+	 * @since 4.4 Get all the data from instance properties rather than function args
+	 */
+	private function is_geofenced() {
+		if ( is_null( $this->latitude ) || is_null( $this->longitude ) || is_null( $this->user_id ) ) {
+			return false;
+		}
+		$geofences = get_user_meta( $this->user_id, 'ts_geofences', true );
+		if ( ! is_array( $geofences ) ) {
+			return false;
+		}
+		foreach ( $geofences as $i => $fence ) {
+			if ( $fence['radius'] > 0 ) {
+				$lat1     = (float) $fence['lat'];
+				$lon1     = (float) $fence['lon'];
+				$lat2     = (float) $this->latitude;
+				$lon2     = (float) $this->longitude;
+				$distance = $this->trackserver->distance( $lat1, $lon1, $lat2, $lon2 );
+				if ( $distance <= $fence['radius'] ) {
+					return $fence['action'];
+				}
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Save the location to the database.
+	 *
+	 * If the instance doesn't have an ID, insert a new location, otherwise update
+	 * the existing one. Returns true on success, or false on failure.
 	 *
 	 * @since 4.4
 	 */
 	public function save() {
 		global $wpdb;
 
-		$data	= array(
+		$fenced = $this->is_geofenced();
+		if ( $fenced === 'discard' ) {
+			return true;
+		}
+
+		$data   = array(
 			'trip_id'   => $this->track_id,
 			'latitude'  => $this->latitude,
 			'longitude' => $this->longitude,
@@ -53,7 +88,7 @@ class Trackserver_Location {
 			'created'   => $this->created,
 			'occurred'  => $this->occurred,
 			'comment'   => $this->comment,
-			'hidden'    => $this->hidden,
+			'hidden'    => ( $fenced === 'hide' ? 1 : 0 ),
 		);
 		$format = array( '%d', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%d' );
 
@@ -64,16 +99,14 @@ class Trackserver_Location {
 
 			if ( $wpdb->insert( $this->trackserver->tbl_locations, $data, $format ) ) {
 				$this->id = $wpdb->insert_id;
-				return $this->id;
+				return true;
 			}
-			var_dump($wpdb);
-
 		} else {
 
 			$where     = array( 'id' => $this->id );
 			$where_fmt = array( '%d' );
 			if ( $wpdb->update( $this->trackserver->tbl_locations, $data, $where, $format, $where_fmt ) ) {
-				return $this->id;
+				return true;
 			}
 		}
 		return false;
