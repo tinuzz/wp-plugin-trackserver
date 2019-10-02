75ad39fc4d1f76d64a0c800a290ec4d0e199a224
Martijn Grendelman <m@rtijn.net>
2019-10-02 22:08:23 +0200

    Enhance 'get' function for other primary lookups in Trackserver_Track

--- class-trackserver-track.php
+++ class-trackserver-track.php
@@ -1,7 +1,7 @@
 <?php
 
 if ( ! defined( 'TRACKSERVER_PLUGIN_DIR' ) ) {
-	  die( 'No, sorry.' );
+	die( 'No, sorry.' );
 }
 
 class Trackserver_Track {
@@ -23,32 +23,53 @@ class Trackserver_Track {
 	 *
 	 * @since 4.4
 	 */
-	public function __construct( $trackserver, $id = null, $user_id = null ) {
+	public function __construct( $trackserver, $id = null, $user_id = null, $restrict = true ) {
 		$this->trackserver = $trackserver;
 		$this->user_id     = (int) $user_id;
 
 		if ( ! ( is_null( $id ) || is_null( $user_id ) ) ) {
-			$this->get( (int) $id, (int) $user_id );
+			$this->get_by( 'id', (int) $id, (int) $user_id, $restrict );
 		}
 	}
 
 	/**
-	 * Get a track from the database.
+	 * Get a track from the database by diffent fields.
 	 *
-	 * Given a track ID and a user ID, get the track's properties and store them
-	 * on this object.  Returns true on success or false on failure.
+	 * Given a track ID or name and a user ID, get the track's properties and
+	 * store them on this object. Returns true on success or false on failure.
+	 *
+	 * If $restrict is true or $field == 'name', the track must be owned by the specified user.
+	 * If $restrict is false and $field == 'id', the request is satisfied if the user can 'trackserver_publish'.
 	 *
 	 * @since 4.4
 	 */
-	private function get( $id, $user_id ) {
+	public function get_by( $field, $value, $user_id, $restrict = true ) {
 		global $wpdb;
 
-		if ( user_can( $user_id, 'trackserver_publish' ) ) {
-			$sql = $wpdb->prepare( 'SELECT * FROM ' . $this->trackserver->tbl_tracks . ' WHERE id=%d', $id );
-		} else {
-			$sql = $wpdb->prepare( 'SELECT * FROM ' . $this->trackserver->tbl_tracks . ' WHERE id=%d AND user_id=%d;', $id, $user_id );
+		switch ( $field ) {
+			case 'id':
+				$db_column = 'id';
+				if ( ! user_can( $user_id, 'trackserver_publish' ) ) {
+					$restrict = true;   // Require user_id to match if the user lacks capability
+				}
+				break;
+			case 'name':
+				$db_column = 'name';
+				$restrict  = true;    // Require user_id to match for name-based requests
+				break;
+			default:
+				return false;
 		}
-		$row = $wpdb->get_row( $sql, ARRAY_A );
+
+		if ( $restrict === false ) {
+			// phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared,WordPress.DB.PreparedSQL.NotPrepared
+			$sql = $wpdb->prepare( 'SELECT * FROM ' . $this->trackserver->tbl_tracks . " WHERE $db_column = %s ORDER BY updated DESC LIMIT 1", $value );
+		} else {
+			// phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared,WordPress.DB.PreparedSQL.NotPrepared
+			$sql = $wpdb->prepare( 'SELECT * FROM ' . $this->trackserver->tbl_tracks . " WHERE user_id=%d AND $db_column = %s ORDER BY updated DESC LIMIT 1", $user_id, $value );
+		}
+
+		$row = $wpdb->get_row( $sql, ARRAY_A );  // phpcs:ignore WordPress.DB.PreparedSQL.NotPrepared
 		if ( ! is_null( $row ) ) {
 			$this->id       = (int) $row['id'];
 			$this->user_id  = (int) $row['user_id'];
@@ -73,12 +94,12 @@ class Trackserver_Track {
 	public function save() {
 		global $wpdb;
 
-		$data	= array(
+		$data   = array(
 			'user_id'  => $this->user_id,
-			'name'		 => $this->name,
+			'name'     => $this->name,
 			'created'  => $this->created,
-			'source'	 => $this->source,
-			'comment'	 => $this->comment,
+			'source'   => $this->source,
+			'comment'  => $this->comment,
 			'distance' => $this->distance,
 		);
 		$format = array( '%d', '%s', '%s', '%s', '%s', '%d' );
@@ -92,7 +113,6 @@ class Trackserver_Track {
 				$this->id = $wpdb->insert_id;
 				return $this->id;
 			}
-
 		} else {
 
 			$where     = array( 'id' => $this->id );
