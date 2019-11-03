4b4d1cc7825668ce9552168ff17c6139477ee7dd
Martijn Grendelman <m@rtijn.net>
2019-11-03 23:20:03 +0100

    Move most MapMyTracks functions to Trackserver_Mapmytracks class

--- class-trackserver-mapmytracks.php
+++ class-trackserver-mapmytracks.php
@@ -38,16 +38,16 @@ class Trackserver_Mapmytracks {
 
 		switch ( $_POST['request'] ) {
 			case 'start_activity':
-				$this->trackserver->handle_mapmytracks_start_activity( $this->user_id );
+				$this->handle_start_activity( $this->user_id );
 				break;
 			case 'update_activity':
-				$this->trackserver->handle_mapmytracks_update_activity( $this->user_id );
+				$this->handle_update_activity( $this->user_id );
 				break;
 			case 'stop_activity':
-				$this->trackserver->handle_mapmytracks_stop_activity( $this->user_id );
+				$this->handle_stop_activity( $this->user_id );
 				break;
 			case 'upload_activity':
-				$this->trackserver->handle_mapmytracks_upload_activity( $this->user_id );
+				$this->handle_upload_activity( $this->user_id );
 				break;
 			default:
 				$this->trackserver->http_terminate( 501, 'Unsupported MapMyTracks request.' );
@@ -83,4 +83,175 @@ class Trackserver_Mapmytracks {
 		echo str_replace( array( "\r", "\n" ), '', $xml->asXML() );
 	}
 
-}  // class
+	/**
+	 * Get the source app and version from a MapMyTracks request.
+	 */
+	function get_source() {
+		$source = '';
+		if ( array_key_exists( 'source', $_POST ) ) {
+			$source .= $_POST['source'];
+		}
+		if ( array_key_exists( 'version', $_POST ) ) {
+			$source .= ' v' . $_POST['version'];
+		}
+		if ( $source != '' ) {
+			$source .= ' / ';
+		}
+		$source .= 'MapMyTracks';
+		return $source;
+	}
+
+	/**
+	 * Handle the 'start_activity' request for the MapMyTracks protocol.
+	 *
+	 * If no title / track name is received, an error is sent, otherwise track
+	 * is saved, received points are validated, valid points are inserted and
+	 * and the new trip ID is sent in an XML reponse.
+	 *
+	 * @since 1.0
+	 * @since 4.4 Delegate DB access to Trackserver_Track instance
+	 */
+	function handle_start_activity( $user_id ) {
+		if ( ! empty( $_POST['title'] ) ) {
+
+			$track = new Trackserver_Track( $this->trackserver, null, $user_id );
+			$track->set( 'name', $_POST['title'] );
+			$track->set( 'source', $this->get_source() );
+
+			if ( $track->save() ) {
+				list( $result, $reason ) = $this->process_points( $track->id, $user_id );
+				if ( $result ) {
+					return $this->send_response( 'activity_started', array( 'activity_id' => (string) $track->id ) );
+				}
+				return $this->error_response( $reason );
+			}
+		}
+		return $this->error_response( 'input error' );
+	}
+
+	/**
+	 * Handle 'update_activity' request for the MapMyTracks protocol.
+	 *
+	 * It tries to instantiate a track object from the given activity_id. If that succeeds,
+	 * the location data is parsed and added to the track.
+	 *
+	 * @since 1.0
+	 */
+	function handle_update_activity( $user_id ) {
+		$track	 = new Trackserver_Track( $this->trackserver, $_POST['activity_id'], $user_id );	// $restrict = true
+		if ( $track->id ) {
+			list( $result, $reason ) = $this->process_points( $track->id, $user_id );
+			if ( $result ) {
+				return $this->send_response( 'activity_updated' );
+			}
+			return $this->error_response( $reason );
+		}
+		return $this->error_response( 'track not found' );
+	}
+
+	/**
+	 * Handle 'stop_activity' request for the MapMyTracks protocol.
+	 *
+	 * This doesn't do anything, except return an appropriate XML message.
+	 *
+	 * @since 1.0
+	 */
+	function handle_stop_activity( $user_id ) {
+		return $this->mapmytracks_response( 'activity_stopped' );
+	}
+
+	/**
+	 * Handle 'upload_activity' request for the MapMyTracks protocol.
+	 *
+	 * It validates and processes the input as GPX data, and returns an
+	 * appropriate XML message.
+	 *
+	 * @since 1.0
+	 */
+	function handle_upload_activity( $user_id ) {
+		global $wpdb;
+
+		$_POST = stripslashes_deep( $_POST );
+		if ( isset( $_POST['gpx_file'] ) ) {
+			$xml = $this->trackserver->validate_gpx_string( $_POST['gpx_file'] );
+			if ( $xml ) {
+				$result = $this->trackserver->process_gpx( $xml, $user_id );
+
+				// If a description was given, put it in the comment field.
+				if ( isset( $_POST['description'] ) ) {
+					$track_ids = $result['track_ids'];
+					if ( count( $track_ids ) > 0 ) {
+						$in	= '(' . implode( ',', $track_ids ) . ')';
+						$sql = $wpdb->prepare( 'UPDATE ' . $this->trackserver->tbl_tracks . " SET comment=%s WHERE user_id=%d AND id IN $in", $_POST['description'], $user_id ); // phpcs:ignore WordPress.DB.PreparedSQL.NotPrepared,WordPress.DB.PreparedSQL.InterpolatedNotPrepared
+						$wpdb->query( $sql ); // phpcs:ignore WordPress.DB.PreparedSQL.NotPrepared
+					}
+				}
+				return $this->send_response( 'success' );
+			}
+		}
+		return $this->error_response( 'Invalid input' );
+	}
+
+	/**
+	 * Process 'points' input from a MapMyTracks request.
+	 *
+	 * Parse the input from $_POST and save the points to the database. On
+	 * errors, abort the operation and inform the caller.
+	 *
+	 * @since 4.4
+	 */
+	function process_points( $track_id, $user_id ) {
+		if ( empty( $_POST['points'] ) ) {
+			return array( true, '' );
+		}
+
+		$points = $this->parse_points( $_POST['points'] );
+
+		if ( $points === false ) {
+			return array( false, 'input error' );
+		}
+
+		if ( $this->trackserver->mapmytracks_insert_points( $points, $track_id, $user_id ) ) {
+			return array( true, '' );
+		}
+
+		return array( false, 'server error' );
+	}
+
+	/**
+	 * Parse a string of points from a MapMyTracks request.
+	 *
+	 * Usinga regular expression, the string of points is parsed into
+	 * an associative array for easier storage in the database.
+	 */
+	function parse_points( $points ) {
+
+		// Check the points syntax. It should match groups of four items. The
+		// first three may contain numbers, dots, dashes and the letter 'E' (it
+		// appears that OruxMaps sometimes sends floats in the scientific
+		// notation, like '7.72E-4'). The last one is the timestamp, which may
+		// only contain numbers.
+		$pattern = '/^([\dE.-]+ [\dE.-]+ [\dE.-]+ [\d]+ ?)*$/';
+		$n			 = preg_match( $pattern, $points );
+
+		if ( $n == 1 ) {
+			$parsed = array();
+			$all		= explode( ' ', $points );
+			for ( $i = 0; $i < count( $all ); $i += 4 ) {
+				if ( $all[ $i ] ) {
+
+					$parsed[] = array(
+						'latitude'	=> number_format( $all[ $i ], 6 ),
+						'longitude' => number_format( $all[ $i + 1 ], 6 ),
+						'altitude'	=> number_format( $all[ $i + 2 ], 6 ),
+						'timestamp' => $all[ $i + 3 ],
+					);
+				}
+			}
+			return $parsed;
+		} else {
+			return false;	// Invalid input
+		}
+	}
+
+} // class
