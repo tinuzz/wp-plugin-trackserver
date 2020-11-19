96cf6fe9782e2a30a8a46a158ae444554b952a45
Martijn Grendelman <m@rtijn.net>
2020-11-19 11:01:02 +0100

    Set 'user-scalable=no' for embedded maps

--- embedded-template.php
+++ embedded-template.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 		<title><?php echo the_title(); ?></title>
 		<style>
 			body {
