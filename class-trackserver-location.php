49a187df1aeab6773e283aa404083a0a6cdae924
Martijn Grendelman <m@rtijn.net>
2022-01-20 14:00:06 +0100

    Remove duplicate method is_geofenced()

--- class-trackserver-location.php
+++ class-trackserver-location.php
@@ -41,29 +41,14 @@ class Trackserver_Location {
 	 * Function to check whether this location is geofenced.
 	 *
 	 * @since 3.1
-	 * @since 5.0 Get all the data from instance properties rather than function args
+	 * @since 5.0 Delegate to the function in Trackserver main class
 	 */
 	private function is_geofenced() {
 		if ( is_null( $this->latitude ) || is_null( $this->longitude ) || is_null( $this->user_id ) ) {
 			return false;
 		}
-		$geofences = get_user_meta( $this->user_id, 'ts_geofences', true );
-		if ( ! is_array( $geofences ) ) {
-			return false;
-		}
-		foreach ( $geofences as $i => $fence ) {
-			if ( $fence['radius'] > 0 ) {
-				$lat1     = (float) $fence['lat'];
-				$lon1     = (float) $fence['lon'];
-				$lat2     = (float) $this->latitude;
-				$lon2     = (float) $this->longitude;
-				$distance = $this->trackserver->distance( $lat1, $lon1, $lat2, $lon2 );
-				if ( $distance <= $fence['radius'] ) {
-					return $fence['action'];
-				}
-			}
-		}
-		return false;
+		$data = [ 'latitude' => $this->latitude, 'longitude' => $this->longitude ];
+		return $this->trackserver->is_geofenced( $this->user_id, $data );
 	}
 
 	/**
